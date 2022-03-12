f0935d06b723204d0abb42a8aebae2bedba3ab12
Josephine Chan <71271059+JC32101@users.noreply.github.com>
2022-03-11 22:30:13 -0500

    Calculation Updated

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -1,4 +1,3 @@
-# track how much data was sent during that time frame
 import sys
 import socket
 import time
@@ -8,34 +7,26 @@ if (len(sys.argv) != 4):
 elif (int(sys.argv[2]) < 1025 or int(sys.argv[2]) > 65534):
     print("Error: port number must be in the range 1024 to 65535")
 
-serverName = sys.argv[1]
-serverPort = int(sys.argv[2])
-time = int(sys.argv[3])
-serverAddress = (serverName, serverPort)
-bit = 0
-message = "1"
+ServerName = sys.argv[1]
+ServerPort = int(sys.argv[2])
+Time = int(sys.argv[3])
+ServerAddress = (ServerName, ServerPort)
+byte = 0
+message = bytes(1000)
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 try:
-  clientSocket.connect(serverAddress)
-  endtime = time.time() + time
+  clientSocket.connect(ServerAddress)
+  endtime = time.time() + Time
   while (time.time() < endtime):
-    
-    # Send message
     clientSocket.send(message.encode("utf-8"))
-    bit=bit+1000
     
-    # Receive from server
     modified_sent = clientSocket.recvfrom(2048)
-
-  # Close connection
-  clientSocket.close()
     
-  #Print Bandwidth
-  bandwidth = (bit/1000000)/time
-  print("Bandwidth: " + bandwidth + " Mbps")
+    byte=len(modified_sent[0])
+  Throughput = byte/(Time*125000)
+  print("sent="+str(bit/1000)+"KB rate="+str(Throughput)+"Mbps")
 except socket.error as err:
   print(err)
 
-
+clientSocket.close()
